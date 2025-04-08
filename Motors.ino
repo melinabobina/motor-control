d8fed2cb7e92711aca7a6ee125b5a54be2a394d4
Mletzy <165437625+Mletzy@users.noreply.github.com>
2025-04-07 22:22:41 -0500

    Update Motors.ino

--- Motors.ino
+++ Motors.ino
@@ -1,185 +1,140 @@
 /*
 
       Motor Driver Program:
-      Controls 9 DM542T stepper motor drivers for the Nerual Kinetic Sculpture
-  
-      Modified: March 12, 2025
-      By: Letzy Mota and Karina Montero
+      Controls 3, 6, or 9 DM542T stepper motor drivers for the Nerual Kinetic Sculpture
+
+      Modified: April 7, 2025
+      By: Letzy Mota
+      Note: this codde might not fully work but it compiled so ill test the logic at a later date :p
 
 */ 
 
-#define PulPin1 2 // pin 2 will control motor 1s pulse 
-#define DirPin1 3 // pin 3 will control motor 1s direction
+#define PulPin0 2 // pin 2 will control motor 1s pulse 
+#define DirPin0 3 // pin 3 will control motor 1s direction
 
-#define PulPin2 4 // pin 4 will control motor 2s pulse
-#define DirPin2 5 // pin 5 will control motor 2s direction
+#define PulPin1 4 // pin 4 will control motor 2s pulse
+#define DirPin1 5 // pin 5 will control motor 2s direction
 
-#define PulPin3 6 // pin 6 will control motor 3s pulse
-#define DirPin3 7 // pin 7 will control motor 3s direction
+#define PulPin2 6 // pin 6 will control motor 3s pulse
+#define DirPin2 7 // pin 7 will control motor 3s direction
 
-#define PulPin4 8 // pin 8 will control motor 4s pulse
-#define DirPin4 9 // pin 9 will control motor 4s direction
+#define PulPin3 8 // pin 8 will control motor 4s pulse
+#define DirPin3 9 // pin 9 will control motor 4s direction
 
-#define PulPin5 10 // pin 10 will control motor 5s pulse
-#define DirPin5 11 // pin 11 will control motor 5s direction
+#define PulPin4 10 // pin 10 will control motor 5s pulse
+#define DirPin4 11 // pin 11 will control motor 5s direction
 
-#define PulPin6 12 // pin 12 will control motor 6s pulse
-#define DirPin6 13 // pin 13 will control motor 6s direction
+#define PulPin5 12 // pin 12 will control motor 6s pulse
+#define DirPin5 13 // pin 13 will control motor 6s direction
 
-#define PulPin7 22 // pin 22 will control motor 7s pulse
-#define DirPin7 23 // pin 23 will control motor 7s direction
+#define PulPin6 22 // pin 22 will control motor 7s pulse
+#define DirPin6 23 // pin 23 will control motor 7s direction
 
-#define PulPin8 24 // pin 24 will control motor 8s pulse
-#define DirPin8 25 // pin 25 will control motor 8s direction
+#define PulPin7 24 // pin 24 will control motor 8s pulse
+#define DirPin7 25 // pin 25 will control motor 8s direction
 
-#define PulPin9 26 // pin 26 will control motor 9s pulse
-#define DirPin9 27 // pin 27 will control motor 9s direction 
+#define PulPin8 26 // pin 26 will control motor 9s pulse
+#define DirPin8 27 // pin 27 will control motor 9s direction 
 
-void Motor1Control(int Direction, int Delay);  
-void Motor2Control(int Direction, int Delay);
-void Motor3Control(int Direction, int Delay);
-void Motor4Control(int Direction, int Delay);  
-void Motor5Control(int Direction, int Delay);
-void Motor6Control(int Direction, int Delay);
-void Motor7Control(int Direction, int Delay);  
-void Motor8Control(int Direction, int Delay);
-void Motor9Control(int Direction, int Delay);
+int PulPins[] = {PulPin0, PulPin1, PulPin2, PulPin3, PulPin4, PulPin5, PulPin6, PulPin7, PulPin8};
+int DirPins[] = {DirPin0, DirPin1, DirPin2, DirPin3, DirPin4, DirPin5, DirPin6, DirPin7, DirPin8};
+
+// configuration object containing all individual motor parameters the user can map
+struct configuration{
+  int PulPin;
+  int DirPin;
+};
+
+void motorControl(int MotorNum, int Direction, int Delay);
+
+/* TO DO : create function to get all panels to starting height */
+void startingHeight(void);
+
+// app input for all motors
+int startHeight; 
+int rows; 
+int columns;
+
+// app input for individual motors
+int rpos; 
+int cpos;
+int speed; 
+int direction; 
+
+configuration** MotorArr;
 
 void setup(){
 
-// configure input pins for app instructions??? (analog???)
-// ... ???
-// read configuration and save instructions (analog to map or switch???)
-// ... ???
+// initialize UART communication (pin 19 -> RX1)
+  Serial1.begin(9600); 
 
-// configure pins for filtered EEG input (analog) [idk how many channels?]
-// ...
+// wait unitl data is received
+  while(!Serial1.available()){}
 
-// configure pins to output pulse and direction (digital)
-int PulPins[] = {PulPin1, PulPin2, PulPin3, PulPin4, PulPin5, PulPin6, PulPin7, PulPin8, PulPin9};
-int DirPins[] = {DirPin1, DirPin2, DirPin3, DirPin4, DirPin5, DirPin6, DirPin7, DirPin8, DirPin9};
+// read configuration data
+  startHeight = Serial1.parseInt();
+  rows = Serial1.parseInt();
+  columns = Serial1.parseInt();
 
-  for(int i=0; i<9; i++){
-    pinMode(PulPins[i], OUTPUT);
-    pinMode(DirPins[i], OUTPUT);
+// allocate memory for rows
+  MotorArr = new configuration*[rows];
+// allocate memory for each column in each row
+  for(int i = 0; i < rows; i++){
+    MotorArr[i] = new configuration[columns];
+  }
+
+  int idx = 0;
+  for(int i = 0; i<rows; i++){
+    for(int j = 0; j<columns; j++){
+      MotorArr[i][j].PulPin = PulPins[idx]; // asign pulse pin to matrix poition
+      MotorArr[i][j].DirPin = DirPins[idx]; // asign direction pin to matrix position
+
+      pinMode(MotorArr[i][j].PulPin, OUTPUT); // configure pulse pin to output
+      pinMode(MotorArr[i][j].DirPin, OUTPUT); // configure direction pin to output
+
+      idx++;
+    }
   }
 
 }
 
 void loop(){
 
-// read the input signal (analog)
-// ...
+  if(Serial1.available() >= 8){
+// read data
+    rpos = Serial1.parseInt();
+    cpos = Serial1.parseInt();
+    speed = Serial1.parseInt();
+    direction = Serial1.parseInt();
 
-// execute motor instructions 
-  Motor1Control(1,500); //0 -> clockwise, 1-> counterclockwise 
-  Motor2Control(1,50);
-  Motor3Control(1,50);
-  Motor4Control(1,50);
-  Motor5Control(1,50);
-  Motor6Control(1,50);
-  Motor7Control(1,50);
-  Motor8Control(1,50);
-  Motor9Control(1,50);
+// execute motor instructions (5 pulses per configuration)
+    motorControl(rpos, cpos, direction, speed);
+    delay(10);
+    motorControl(rpos, cpos, direction, speed);
+    delay(10);
+    motorControl(rpos, cpos, direction, speed);
+    delay(10); 
+    motorControl(rpos, cpos, direction, speed);
+    delay(10);
+    motorControl(rpos, cpos, direction, speed);
+    delay(10);
+  }
 
 }
 
-// Functions to 
-void Motor1Control(int Direction, int Delay){
+void motorControl(int rpos, int cpos, int Direction, int Delay){
+// for direction: 0 -> clockwise, 1-> counterclockwise 
 
-  digitalWrite(DirPin1, Direction);
+  digitalWrite(MotorArr[rpos][cpos].DirPin, Direction);
 
-  digitalWrite(PulPin1, HIGH);
+  digitalWrite(MotorArr[rpos][cpos].PulPin, HIGH);
   delayMicroseconds(Delay);
-  digitalWrite(PulPin1, LOW);
-  delayMicroseconds(Delay);
-
-}  
-
-void Motor2Control(int Direction, int Delay){
-
-  digitalWrite(DirPin2, Direction);
-
-  digitalWrite(PulPin2, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin2, LOW);
+  digitalWrite(MotorArr[rpos][cpos].PulPin, LOW);
   delayMicroseconds(Delay);
 
 }
 
-void Motor3Control(int Direction, int Delay){
-
-  digitalWrite(DirPin3, Direction);
-
-  digitalWrite(PulPin3, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin3, LOW);
-  delayMicroseconds(Delay);
-
-}
-
-void Motor4Control(int Direction, int Delay){
-
-  digitalWrite(DirPin4, Direction);
-
-  digitalWrite(PulPin4, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin4, LOW);
-  delayMicroseconds(Delay);
-
-}  
-
-void Motor5Control(int Direction, int Delay){
-
-  digitalWrite(DirPin5, Direction);
-
-  digitalWrite(PulPin5, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin5, LOW);
-  delayMicroseconds(Delay);
-
-}
-
-void Motor6Control(int Direction, int Delay){
-
-  digitalWrite(DirPin6, Direction);
-
-  digitalWrite(PulPin6, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin6, LOW);
-  delayMicroseconds(Delay);
-
-}
-
-void Motor7Control(int Direction, int Delay){
-
-  digitalWrite(DirPin7, Direction);
-
-  digitalWrite(PulPin7, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin7, LOW);
-  delayMicroseconds(Delay);
-
-}  
-
-void Motor8Control(int Direction, int Delay){
-
-  digitalWrite(DirPin8, Direction);
-
-  digitalWrite(PulPin8, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin8, LOW);
-  delayMicroseconds(Delay);
-
-}
-
-void Motor9Control(int Direction, int Delay){
-
-  digitalWrite(DirPin9, Direction);
-
-  digitalWrite(PulPin9, HIGH);
-  delayMicroseconds(Delay);
-  digitalWrite(PulPin9, LOW);
-  delayMicroseconds(Delay);
+/* TO DO: fill function */
+void startingHeight(void){
 
 }
